99866f7d3b1e3a21e3c3113fc52b6b540d57fe96
Fartoy <55315208+Fartoy@users.noreply.github.com>
2020-12-21 23:17:26 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -86,11 +86,7 @@ dp.add_error_handler(error)
 
 # Start the Bot
 
-updater.start_webhook(listen="0.0.0.0",
-
-port=int(PORT),
-
-url_path=TOKEN)
+updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
 
 updater.bot.setWebhook('https://app-teleg-blabot.herokuapp.com/' + TOKEN)
 
