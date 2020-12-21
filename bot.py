d261a7410e56c5642de28a16528947a16a617e9f
Fartoy <55315208+Fartoy@users.noreply.github.com>
2020-12-21 23:31:38 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,6 @@ import json
 
 import requests
 
-
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -25,74 +24,73 @@ logger = logging.getLogger(__name__)
 TOKEN = '1477909236:AAF8YbI0j-RCHs5MGssjacT0BiEWZ_uDM-Y'
 
 
-def start(update, context):update.message.reply_text('Hi!')
+def start(update, context):
     """Send a message when the command /start is issued."""
-    
+    update.message.reply_text('Hi!')
 
 
 
-def help(update, context):update.message.reply_text('Help!')
+def help(update, context):
     """Send a message when the command /help is issued."""
-    
+    update.message.reply_text('Help!')
 
 
 
-def echo(update, context):update.message.reply_text(update.message.text)
-    """Echo the user message."""
-   
+def echo(update, context):
+    """Echo the user message.""" 
+    update.message.reply_text(update.message.text)
 
 
 
-def error(update, context):logger.warning('Update "%s" caused error "%s"', update, context.error)
+def error(update, context):
     """Log Errors caused by Updates."""
-    
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 
 
-def echo(update, context):update.message.reply_text(update.message.text)
+def echo(update, context):
     """Echo the user message."""
-    
+    update.message.reply_text(update.message.text)
 
 
-
-def main():updater = Updater(TOKEN, use_context=True)
-
+def main():
+    updater = Updater(TOKEN, use_context=True)
 
 
 # Get the dispatcher to register handlers
 
-dp = updater.dispatcher
+    dp = updater.dispatcher
 
 
 
 # on different commands - answer in Telegram
 
-dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("start", start))
 
-dp.add_handler(CommandHandler("help", help))
-
-
+    dp.add_handler(CommandHandler("help", help))
 
 
-dp.add_handler(MessageHandler(Filters.text, echo))
+
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
 
 
 # log all errors
 
-dp.add_error_handler(error)
+    dp.add_error_handler(error)
 
 
 
 # Start the Bot
 
-updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
+    updater.start_webhook(listen="0.0.0.0",port=int(PORT),url_path=TOKEN)
 
-updater.bot.setWebhook('https://app-teleg-blabot.herokuapp.com/' + TOKEN)
+    updater.bot.setWebhook('https://app-teleg-blabot.herokuapp.com/' + TOKEN)
 
 
 
-updater.idle()
+    updater.idle()
 
 
 
